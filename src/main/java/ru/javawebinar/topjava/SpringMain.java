70099ddc995063773317c6c46d7f91913add967a
Nikko <Niko-ikko@ya.ru>
2016-09-19 21:10:53 +0400

    HW2 +optional

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -13,12 +13,5 @@ import java.util.Arrays;
  * Date: 22.08.2014
  */
 public class SpringMain {
-    public static void main(String[] args) {
-        // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(1, "userName", "email", "password", Role.ROLE_ADMIN));
-        }
-    }
+
 }
